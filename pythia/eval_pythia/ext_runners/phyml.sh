75c08b408e9bdc1eba13a7a62dd6a6fba3b111b4
lucasperception <18529607+lucasperception@users.noreply.github.com>
2025-03-23 17:57:45 +0100

    Add more or less comparable calls to phyml / raxml

--- pythia/eval_pythia/ext_runners/phyml.sh
+++ pythia/eval_pythia/ext_runners/phyml.sh
@@ -1,1 +1,12 @@
-/bin/phyml -i $1 -d nt -m GTR -a e
+temp_dir=$(mktemp -d)
+cp "$1" "$temp_dir"/
+cd "$temp_dir" || exit
+if [ "$2" = "nucleotide" ]; then
+  echo "Running nucleotide analysis for $1"
+  /bin/phyml -i "$(basename "$1")" -d nt -m GTR -f e -c 2 -a 1 -o tl
+elif [ "$2" = "protein" ]; then
+  echo "Running protein analysis for $1"
+  /bin/phyml -i "$(basename "$1")" -d aa -m LG -f e -c 2 -a 1 -o tl
+fi
+cd - || exit
+rm -rf "$temp_dir"
--- pythia/eval_pythia/ext_runners/raxml.sh
+++ pythia/eval_pythia/ext_runners/raxml.sh
@@ -1,1 +1,12 @@
-/bin/raxml-ng --search1 --msa $1 --model GTR+G --nofiles --log ERROR
+temp_dir=$(mktemp -d)
+cp "$1" "$temp_dir"/
+cd "$temp_dir" || exit
+if [ "$2" = "nucleotide" ]; then
+  echo "Running nucleotide analysis for $1"
+  /bin/raxml-ng --msa "$(basename "$1")" --tree random{1} --model GTR+G --opt-branches off
+elif [ "$2" = "protein" ]; then
+  echo "Running protein analysis for $1"
+  /bin/raxml-ng --msa "$(basename "$1")" --tree random{1} --model LG+G --opt-branches off
+fi
+cd - || exit 1
+rm -rf "$temp_dir"
